971a3ca67a2256088d9617ae2cfcbe7ca5074b1c
blackbirdtxt <oscarsitoequisde@gmail.com>
2026-01-09 17:50:22 -0500

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dfac7ac2d86d326a0e5adb024e7943c181393ed17a5fcb8f0315b24c7da6ddde
-size 94
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;
